bcb5aa151e6bffb85fcf622dca87fce956ef3647
sschaening <staxschaening@gmail.com>
2017-08-02 00:45:05 +1000

    new AI, replaced old folder with new. not yet complete prototype but its there

---
+++ Ai Prototype/Assets/Code/Old Scripts/chase.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class chase : MonoBehaviour {
+
+
+
+
+
+
+
+	public Transform player;
+	 Animator anim;
+	public Transform head;
+
+
+	public float Following = 10.0f;
+	public float Attacking = 5.0f;
+	public float Degreesight = 30;
+
+
+	//bool chasing = false;
+
+	string state = "patrol";
+	public GameObject[] waypoints;
+	int currentWP = 0;
+	public float rotSpeed = 0.2f;
+	public float speed = 1.5f;
+	public float accuarcyWP = 5.0f;
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		SearchandFollow ();
+
+	
+	}
+
+	void Pathfinding(){
+		
+
+
+
+	}
+
+
+
+	void SearchandFollow()
+	{
+		
+		Vector3 direction = player.position - this.transform.position;
+		direction.y = 0;
+		float angle = Vector3.Angle (direction, this.head.up);
+
+
+
+
+
+		if (state == "patrol" && waypoints.Length > 0) 
+		{
+			anim.SetBool ("Idle", false);
+			anim.SetBool ("Walking", true);
+			if (Vector3.Distance (waypoints [currentWP].transform.position, transform.position) < accuarcyWP)
+			{
+
+
+				currentWP = Random.Range (0, waypoints.Length);
+
+				//one after another 
+				/*currentWP++;
+				if (currentWP >= waypoints.Length) 
+				{
+					currentWP = 0;
+				}*/
+			}
+			direction  = waypoints [currentWP].transform.position - transform.position;
+			this.transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), rotSpeed * Time.deltaTime);
+			this.transform.Translate (0, 0, Time.deltaTime * speed);
+		}
+
+
+		if (Vector3.Distance (player.position, this.transform.position) <  Following && (angle <  Degreesight || state == "chasing")) 
+		{
+			
+			state = "chasing";
+			//chasing = true;
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
+
+			//anim.SetBool ("Idle", false);
+		
+			if (direction.magnitude > Attacking)
+				{
+				
+				this.transform.Translate (0, 0, Time.deltaTime * speed);
+				anim.SetBool ("Walking", true);
+				anim.SetBool ("Attack", false);
+
+
+				} 
+				else 
+					{
+			
+						anim.SetBool ("Attack", true);
+						anim.SetBool ("Walking", false);
+				
+					}
+		
+		
+		} 
+		else 
+		{
+			//anim.SetBool ("Idle", true);
+			anim.SetBool ("Walking", true);
+			anim.SetBool ("Attack", false);
+			//chasing = false;
+			state = "patrol";
+		}
+
+
+
+	}
+
+
+}
---
+++ Ai Prototype/Assets/Code/TestScript.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class TestScript : MonoBehaviour
+    {
+
+        public NavMeshAgent agent;
+        public ThirdPersonCharacter character;
+
+        public enum State
+        {
+            PATROL,
+            CHASE
+
+
+        }
+
+        public State state;
+        private bool alive;
+
+        //Variable for patrolling
+        public GameObject[] waypoints;
+        private int waypointInd;
+        public float patrolSpeed = 0.5f;
+        
+
+
+        //Variables for Chasing
+
+        public float chaseSpeed = 1f;
+        public GameObject target;
+
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updatePosition = true;
+            agent.updateRotation = false;
+
+            waypoints = GameObject.FindGameObjectsWithTag("Waypoints");
+            waypointInd = Random.Range(0, waypoints.Length);
+            state = TestScript.State.PATROL;
+
+            alive = true;
+            // Start FSM
+            StartCoroutine(FSM ());
+
+
+            
+
+
+        }
+
+        IEnumerator FSM()
+        {
+            while (alive)
+            {
+                switch (state)
+                {
+                    case State.PATROL:
+                        Patrol();
+                        break;
+                    case State.CHASE:
+                        Chase();
+                        break;
+
+                }
+                yield return null;
+            }
+        }
+
+        void Patrol()
+        {
+            agent.speed = patrolSpeed;
+            if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) >= 2)
+            {
+
+                agent.SetDestination(waypoints[waypointInd].transform.position);
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else if (Vector3.Distance (this.transform.position, waypoints[waypointInd].transform.position) <=2)
+            {
+                waypointInd = Random.Range(0, waypoints.Length);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+        void Chase()
+        {
+            agent.speed = chaseSpeed;
+            agent.SetDestination(target.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                state = TestScript.State.CHASE;
+                target = coll.gameObject;
+            }
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
