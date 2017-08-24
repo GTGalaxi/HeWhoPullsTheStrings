d9303a9bec6f0bd017dfdd6d4fe5d32661b471d3
Jack Floyd <jackadamfloyd@gmail.com>
2017-08-25 02:36:58 +1000

    magnet rune

---
+++ Major Work/Major work prototyping/Assets/Scripts/Key.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+    public bool activated = false;
+    public float magnetRange =5;
+    public Transform target;
+
+    public MagnetRune magnetrune;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector3.Distance(transform.position, target.position);
+        print(distance);
+
+        if (magnetrune.collectMRune == true && Input.GetKey(KeyCode.F))
+        {
+
+            activated = true;
+
+        }
+
+
+        if (distance < magnetRange)
+        {
+            if (activated == true)
+            {
+                transform.LookAt(player.transform.position);
+
+                transform.position += transform.forward * speed * Time.deltaTime;
+            }
+        }
+    }
+
+}
