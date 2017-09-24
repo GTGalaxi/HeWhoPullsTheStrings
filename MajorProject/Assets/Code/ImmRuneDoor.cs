4e350e3f4bf70d29ed8d013137236123932c4963
GTGalaxi <gtgalaxiyt@gmail.com>
2017-09-25 01:23:13 +1000

    made kingdom layout and magnet garden level

---
+++ MajorProject/Assets/Code/ImmRuneDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmRuneDoor : MonoBehaviour {
+
+
+    public bool doorReady = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+        {
+
+            doorReady = true;
+
+        }
+	}
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKey(KeyCode.E) && doorReady == true)
+        {
+
+            Application.LoadLevel(2);
+
+        }
+    }
+
+
+}
