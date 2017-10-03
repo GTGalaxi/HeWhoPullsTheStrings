f9b77d6e94593933bc95c0d91ac09a70ed8078c2
Mitchell Smith <msmith5@sae.au>
2017-10-03 11:01:52 +1100

    Stuff

--- Tutorial level greybox - project/Assets/CameraScript.cs
+++ Tutorial level greybox - project/Assets/CameraScript.cs
@@ -8,6 +8,7 @@ public class CameraScript : MonoBehaviour {
     public GameObject rotator;
     public GameObject player;
     public GameObject hitBox;
+    public GameObject scarabObject;
 	public int Speed = 5;
     public bool scarab = false;
     public float timer = 0;
@@ -26,20 +27,27 @@ public class CameraScript : MonoBehaviour {
 	
 	void Update ()
     {
+
+        Vector3 Direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Direction = Camera.main.transform.TransformDirection(Direction);
+
         if (!scarab)
         {
             print(false);
-            transform.RotateAround(rotator.transform.position, rotator.transform.up, Speed * Input.GetAxis("Mouse X"));
             rotator.transform.RotateAround(rotator.transform.position, rotator.transform.up, Speed * Input.GetAxis("Mouse X"));
             transform.RotateAround(rotator.transform.position, rotator.transform.right, Speed * Input.GetAxis("Mouse Y"));
-            transform.eulerAngles.Set(transform.eulerAngles.x, 0, transform.eulerAngles.z);
+            float temp = Mathf.Clamp(transform.eulerAngles.x, -70, 70);
+            transform.eulerAngles.Set(temp, transform.rotation.y, transform.rotation.z);
+            print(Mathf.Clamp(transform.eulerAngles.x, -70F, 70F));
         }
         else
         {
             print(true);
             ScarabVision();
             transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0));
-            transform.localRotation.Set(transform.rotation.x, transform.rotation.y, 0, transform.rotation.w);
+            transform.eulerAngles.Set(transform.rotation.x, transform.rotation.y, rotator.transform.rotation.z);
+            scarabObject.transform.position = (scarabObject.transform.position + Direction * Time.deltaTime);
+            transform.position = scarabObject.transform.position;
         }
 
 
@@ -63,7 +71,7 @@ public class CameraScript : MonoBehaviour {
             player.GetComponent<Player_Movement>().enabled = true;
             transform.position = cameraAt.transform.position;
             transform.LookAt(rotator.transform);
-            transform.localRotation.Set(transform.rotation.x, transform.rotation.y, 0, transform.rotation.w);
+            transform.eulerAngles.Set(transform.rotation.x, transform.rotation.y, rotator.transform.rotation.z);
 
         }
     }
