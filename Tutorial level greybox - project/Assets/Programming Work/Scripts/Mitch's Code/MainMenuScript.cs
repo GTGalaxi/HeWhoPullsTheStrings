2c98ca87a68c08f6fbce6e5632b9985d13116e67
Mitchell Smith <gtgalaxiyt@gmail.com>
2017-10-24 00:36:45 +1100

    Menu and Pause UI and Scripts

---
+++ Tutorial level greybox - project/Assets/Programming Work/Scripts/Mitch's Code/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public void QuitButton ()
+    {
+        Application.Quit();
+	}
+
+    public void StartButton ()
+    {
+        Application.LoadLevel(1);
+	}
+
+    public void OptionsButton()
+    {
+        print("THERE ARE NO OPTIONS");
+    }
+}
---
+++ Tutorial level greybox - project/Assets/Programming Work/Scripts/Mitch's Code/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public void ResumeButton()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void ExitButton()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void OptionsButton()
+    {
+        print("THERE ARE NO OPTIONS");
+    }
+}
