abda569e9a41e0fe4f5a2ea319ffc0527530d7c5
Jack Floyd <jfloyd@sae.au>
2017-11-21 10:10:04 +1100

    stuff

--- Tutorial level greybox - project/Assets/Programming Work/Scripts/JacksCode/Collectingrune.cs
+++ Tutorial level greybox - project/Assets/Programming Work/Scripts/JacksCode/Collectingrune.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 public class Collectingrune : MonoBehaviour {
     
     public static int NoOfRunes = 0;
+    public int NoOfRunes2 = 0;
+    private bool setting;
 
-	// Use this for initialization
-	void Start () {
-	}
+    // Use this for initialization
+    void Start () {
+
+        setting = false;
+        RuneInventory.runesAccessable[0] = false;
+        RuneInventory.runesAccessable[1] = false;
+        RuneInventory.runesAccessable[2] = false;
+        RuneInventory.runesAccessable[3] = false;
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
+        NoOfRunes2 = NoOfRunes;
+
         if (NoOfRunes >= 0)
             RuneInventory.runesAccessable[0] = true;
-        if (NoOfRunes >= 0)
+        if (NoOfRunes >= 1)
             RuneInventory.runesAccessable[1] = true;
-        if (NoOfRunes >= 0)
+        if (NoOfRunes >= 2)
             RuneInventory.runesAccessable[2] = true;
-        if (NoOfRunes >= 0)
+        if (NoOfRunes >= 3)
             RuneInventory.runesAccessable[3] = true;
 
-
+        if(setting == true)
+        {
+            NoOfRunes++;
+            setting = false;
+        }
     }
 
     private void OnCollisionStay(Collision collision)
@@ -35,7 +51,7 @@ public class Collectingrune : MonoBehaviour {
 
             if(Input.GetKeyDown(KeyCode.E))
             {
-                NoOfRunes++;
+                setting = true;
                 Application.LoadLevel(Application.loadedLevel +1);
             }
 
