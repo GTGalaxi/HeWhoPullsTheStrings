59dc53e5e2f6ac5e11843587f86cc3080c995928
Mitchell Smith <gtgalaxiyt@gmail.com>
2017-11-28 05:12:29 +1100

    Removed MajorProject doler for cleaning, Fixed and improved camera

--- Tutorial level greybox - project/Assets/Programming Work/Scripts/Mitch's Code/CameraScript.cs
+++ Tutorial level greybox - project/Assets/Programming Work/Scripts/Mitch's Code/CameraScript.cs
@@ -9,9 +9,17 @@ public class CameraScript : MonoBehaviour {
     public GameObject player;
     public GameObject hitBox;
     public GameObject scarabObject;
+    public GameObject cameraXRotator;
 	public int Speed = 5;
     public bool scarab = false;
     public float timer = 0;
+    public float smoothTime = 5f;
+
+    private Quaternion cameraTargetRotX;
+    private Quaternion cameraTargetRotY;
+    public float xSensitivity = 2f;
+    public float ySensitivity = 4f;
+    public bool smooth = true;
 
     void OnEnable()
     {
@@ -20,35 +28,49 @@ public class CameraScript : MonoBehaviour {
 
     void Start ()
     {
-
+        cameraTargetRotX = cameraXRotator.transform.localRotation;
+        cameraTargetRotY = rotator.transform.localRotation;
         transform.LookAt(rotator.transform);
         transform.position = cameraAt.position;
     }
 	
-	void Update ()
+	void FixedUpdate ()
     {
 
-        Vector3 Direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        Direction = Camera.main.transform.TransformDirection(Direction);
+        float yRot = Input.GetAxis("Mouse X") * ySensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * xSensitivity;
+        cameraTargetRotX *= Quaternion.Euler (xRot, 0f, 0f);
+        cameraTargetRotY *= Quaternion.Euler(0f, yRot, 0f);
 
         if (!scarab)
         {
-            //print(false);
-            rotator.transform.Rotate(new Vector3 (0,10,0) * Input.GetAxis("Mouse X"));
-            transform.RotateAround(rotator.transform.position, rotator.transform.right, Speed * Input.GetAxis("Mouse Y"));
-            float temp = Mathf.Clamp(transform.eulerAngles.x, -70, 70);
-            transform.eulerAngles.Set(temp, transform.rotation.y, transform.rotation.z);
+            cameraTargetRotX = ClampRotationAroundXAxis(cameraTargetRotX);
+
+            if (smooth)
+            {
+                rotator.transform.localRotation = Quaternion.Slerp(rotator.transform.localRotation, cameraTargetRotY, smoothTime * Time.deltaTime);
+                cameraXRotator.transform.localRotation = Quaternion.Slerp(cameraXRotator.transform.localRotation, cameraTargetRotX, smoothTime * Time.deltaTime);
+            }
+            else
+            {
+                rotator.transform.localRotation = cameraTargetRotY;
+                cameraXRotator.transform.localRotation = cameraTargetRotX;
+
+            }
         }
+
         else
         {
-            //print(true);
+            Vector3 Direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            Direction = Camera.main.transform.TransformDirection(Direction);
+            
             ScarabVision();
             transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0));
             transform.eulerAngles.Set(transform.rotation.x, transform.rotation.y, rotator.transform.rotation.z);
             scarabObject.transform.position = (scarabObject.transform.position + Direction * 3 * Time.deltaTime);
             transform.position = scarabObject.transform.position;
         }
-
+         
 
         if (Input.GetKey(KeyCode.T))
         {
@@ -73,5 +95,19 @@ public class CameraScript : MonoBehaviour {
             transform.eulerAngles.Set(transform.rotation.x, transform.rotation.y, rotator.transform.rotation.z);
             timer = 0;
         }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+        angleX = Mathf.Clamp(angleX, -20F, 60F);
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
     }
 }
