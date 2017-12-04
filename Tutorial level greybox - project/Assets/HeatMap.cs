5c4c4f6c19a0db9a523aeb0f613a5ca7428419b2
Mitchell Smith <gtgalaxiyt@gmail.com>
2017-12-05 02:42:55 +1100

    HeatMap completed and implemented, still being optimized, Buildings updated, Rune Model added, Controller and rune fixes

---
+++ Tutorial level greybox - project/Assets/HeatMap.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatMap : MonoBehaviour {
+
+    public float colourTime = 0;
+    public float totalTime = 0;
+    [SerializeField]
+    public static float greatestTime = 0;
+    public Gradient heatGradient;
+    public Renderer heatColour;
+
+    // Use this for initialization
+    void Start () {
+        greatestTime = 0;
+
+        GradientColorKey[] gck = new GradientColorKey[5];
+        GradientAlphaKey[] gak = new GradientAlphaKey[5];
+
+        gck[0].color = Color.blue; gck[0].time = 0.0F;
+        gck[1].color = Color.cyan; gck[1].time = 0.25F;
+        gck[2].color = Color.green; gck[2].time = 0.5F;
+        gck[3].color = Color.yellow; gck[3].time = 0.75F;
+        gck[4].color = Color.red; gck[4].time = 1.0F;
+
+        gak[0].alpha = 1.0F; gak[0].time = 0.0F;
+        gak[1].alpha = 1.0F; gak[1].time = 0.25F;
+        gak[2].alpha = 1.0F; gak[2].time = 0.5F;
+        gak[3].alpha = 1.0F; gak[3].time = 0.75F;
+        gak[4].alpha = 1.0F; gak[4].time = 1.0F;
+
+        heatGradient.SetKeys(gck, gak);
+        heatColour = gameObject.GetComponent<Renderer>();
+
+    }
+
+	public void HeatMapUpdate() {
+
+        if (colourTime > greatestTime)
+        {
+            greatestTime = colourTime;
+        }
+        
+        heatColour.material.color = heatGradient.Evaluate((colourTime / greatestTime) * 10);
+        StartCoroutine(stall(1));
+    }
+    
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "VertCollider")
+        {
+            colourTime += Time.deltaTime/2;
+        }
+    }
+
+    IEnumerator stall(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+    }
+
+}
---
+++ Tutorial level greybox - project/Assets/HeatMapGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class heatMapSize
+{
+    public int sizeX = 100;
+    public int sizeZ = 100;
+}
+
+public class HeatMapGenerator : MonoBehaviour {
+
+    public float heatMapResolution = 2;
+    public heatMapSize heatMapSize = new heatMapSize();
+    public GameObject HeatCube;
+    public GameObject[,] HeatCubeArray;
+    public RenderTexture heatMapInput;
+    public Camera heatMapCamera;
+    private bool holder = false;
+
+	// Use this for initialization
+	void Start () {
+        float resolution = 4 / heatMapResolution ;
+        Debug.Log(resolution);
+        heatMapSize.sizeX = heatMapSize.sizeX * Mathf.RoundToInt(resolution + 1);
+        heatMapSize.sizeZ = heatMapSize.sizeZ * Mathf.RoundToInt(resolution + 1);
+        HeatCubeArray = new GameObject[heatMapSize.sizeX, heatMapSize.sizeZ];
+
+        for (int i = -heatMapSize.sizeX/2; i < heatMapSize.sizeX/2; i++)
+        {
+            for (int ii = -heatMapSize.sizeZ/2; ii < heatMapSize.sizeZ/2; ii++)
+            {
+                HeatCubeArray[i + heatMapSize.sizeX / 2, ii + heatMapSize.sizeZ / 2] = Instantiate(HeatCube, new Vector3(gameObject.transform.position.x + i * resolution, gameObject.transform.position.y, gameObject.transform.position.z + ii * resolution), new Quaternion(0, 0, 0, 0));
+                HeatCubeArray[i + heatMapSize.sizeX / 2, ii + heatMapSize.sizeZ / 2].transform.parent = gameObject.transform;
+                HeatCubeArray[i + heatMapSize.sizeX / 2, ii + heatMapSize.sizeZ / 2].transform.localScale = new Vector3(resolution, 4, resolution);
+            }
+        }
+	}
+
+    void UpdateHeatMap ()
+    {
+        for (int i = -heatMapSize.sizeX / 2; i < heatMapSize.sizeX / 2; i++)
+        {
+            for (int ii = -heatMapSize.sizeZ / 2; ii < heatMapSize.sizeZ / 2; ii++)
+            {
+                print(HeatCubeArray[i + heatMapSize.sizeX / 2, ii + heatMapSize.sizeZ / 2]);
+                HeatCubeArray[i + heatMapSize.sizeX / 2, ii + heatMapSize.sizeZ / 2].GetComponent<HeatMap>().HeatMapUpdate();
+            }
+        }
+
+        heatMapCamera.aspect = 1.0f;
+        heatMapCamera.Render();
+
+        RenderTexture.active = heatMapInput;
+        Texture2D heatMapOutput = new Texture2D(2048, 2048, TextureFormat.ARGB32, false);
+
+        heatMapOutput.ReadPixels(new Rect(0, 0, 2048, 2048), 0, 0);
+        RenderTexture.active = null;
+
+        byte[] bytes;
+        bytes = heatMapOutput.EncodeToPNG();
+
+        System.IO.File.WriteAllBytes("C:/tmp/heatMapOutput_"+ SceneManager.GetActiveScene().name +".png", bytes);
+
+    }
+
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.K) && holder == false)
+        {
+            holder = true;
+            UpdateHeatMap();
+            holder = false;
+        }
+    }
+
+    IEnumerator stall(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+    }
+}
